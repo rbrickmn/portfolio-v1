90d289706c834bf8767cf4fd9ddc435eff4bd183
rbrickmn <rbrickmn@gmail.com>
2024-04-03 11:45:34 -0400

    Complete header toggle function & fix bug with headers

--- js/script.js
+++ js/script.js
@@ -18,3 +18,26 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari browser
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera browsers
 }
+
+// When the user interacts with the hamburger menu icon, toggle the mobile header menu
+function toggleMenu() {
+  const mobileHeader = document.querySelector('.mobile-header');
+  if (mobileHeader.style.display === 'flex') {
+    mobileHeader.style.display = 'none';
+  } else {
+    mobileHeader.style.display = 'flex';
+  }
+}
+
+// Detects if the user resized the window while the mobile header is open, solving a bug where the header incorrectly displays after reaching the breakpoint of 820px
+
+window.addEventListener('resize', function() {
+  const mobileHeader = document.querySelector('.mobile-header');
+  const windowWidth = window.innerWidth;
+  const desktopBreakpoint = 820;
+
+  // If the window width is greater than or equal to the desktop breakpoint of 820px, change mobileHeader's display property back to none
+  if (windowWidth >= desktopBreakpoint) {
+    mobileHeader.style.display = 'none';
+  }
+});
